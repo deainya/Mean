79e0139d09811255e90fa52eac0ee77e02872b97
deainya <deainya@gmail.com>
2023-04-18 08:04:01 +0300

    fix: model

--- server/mvc/models/test.js
+++ server/mvc/models/test.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Test = new Schema ({
-    id: { type: Integer, required: true },
+    id: { type: Number, required: true },
     name: { type: String, required: true },
     code: { type: String, required: true },
     desc: { type: String, required: false },
-    date: { type: Integer, required: false }
+    date: { type: Number, required: false }
 });
 
 module.exports = mongoose.model('Test', Test)
